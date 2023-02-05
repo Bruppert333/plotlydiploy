32735314128cd6fe783044bf31aeef208926e9be
Bryce <bryceruppert@gmail.com>
2023-02-05 16:06:48 -0700

    Challenge

--- Starter_Code/charts.js
+++ Starter_Code/charts.js
@@ -61,39 +61,57 @@ function buildCharts(sample) {
     console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0];
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values.slice(0, 10).reverse();
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
 
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = 
+    var yticks = otu_ids.map(IDname => "OTU " + IDname).slice(0, 10).reverse();
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
-    var barData = [
-
+    var barData = [{
+      x: sample_values,
+      y: yticks,
+      type: "bar",
+      orientation: "h",
+      text: otu_labels
+    }
     ];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
-
+      title: "Test Subject OTU's with Bacteria Label"
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-
+    Plotly.newPlot("bar", barData, barLayout)
     // Deliverable 2: 1. Create the trace for the bubble chart.
-
+    var bubbleChart = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: [40, 60, 80, 100],
+        color: ['rgb'],
+        colorscale: 
+        
+      }
+    }]
     // Deliverable 2: 2. Create the layout for the bubble chart.
 
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
